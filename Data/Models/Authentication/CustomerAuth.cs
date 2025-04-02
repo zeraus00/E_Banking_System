76a0ee8ae2b99ffcb823881969d82485cf8d71c5
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-02 15:51:17 +0800

    update CustomerAuth and CustomerAuthConfiguration chore(Data): changed foreign keys and primary key to private set

--- Data/Models/Authentication/CustomerAuth.cs
+++ Data/Models/Authentication/CustomerAuth.cs
@@ -5,20 +5,16 @@
     // CustomersAuth Table
     public class CustomerAuth
     {
-        public int CustomerAuthId { get; }                  // Primary Key
+        public int CustomerAuthId { get; private set; }         // Primary Key
+        public int AccountId { get; private set; }              // Foreign Key to Accounts Table
+        public int UserInfoId { get; private set; }                     // Foreign Key to UsersInfo Table
+        public string UserName { get; set; } = string.Empty;    // Required; Unique
+        public string Email { get; set; } = string.Empty;       // Required; Unique
+        public string Password { get; set; } = string.Empty;    // Hashed password: Required
 
 
-        public int AccountId { get; set; }                  // Foreign Key to Accounts Table
-        public Account Account { get; set; } = null!;       // Navigation Property
 
-
-        public int UserInfoId { get; set; }                 // Foreign Key to UsersInfo Table
-        public UserInfo UserInfo { get; set; } = null!;     // Navigation Property
-
-
-        public string UserName { get; set; } = string.Empty;// Required; Unique
-        public string Email { get; set; } = string.Empty;   // Required; Unique
-        public string Password { get; set; } = string.Empty;// Hashed password: Required
-
+        public Account Account { get; private set; } = null!;   // Navigation Property
+        public UserInfo UserInfo { get; private set; } = null!; // Navigation Property
     }
 }
