fee9192859c6594719a5b72e0b9a57682be655a5
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-04 22:54:54 +0800

    refactor(Data): refactored Configure method and documentations in Address model config

--- Data/Configurations/User/AddressConfiguration.cs
+++ Data/Configurations/User/AddressConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace E_BankingSystem.Data.Configurations.User
+﻿
+
+namespace Data.Configurations.User
 {
     public class AddressConfiguration : IEntityTypeConfiguration<Address>
     {
@@ -6,56 +8,56 @@
         public void Configure(EntityTypeBuilder<Address> Addresses)
         {
             Addresses.ToTable("Addresses", "User");
-            // Define Primary Key
+
+            /*  Configure Table Properties  */
+
+            // AddressId (Primary Key)
             Addresses
                 .HasKey(a => a.AddressId);
             Addresses
                 .Property(a => a.AddressId)
                 .ValueGeneratedOnAdd();
-            // Define MaxLength for House
+            // House (MaxLength=10)
             Addresses
                 .Property(a => a.House)
                 .HasMaxLength(10);
-            // Define MaxLength for Street
+            // Street (MaxLength=50)
             Addresses
                 .Property(a => a.Street)
                 .HasMaxLength(50)
                 .IsRequired();
-
-            // Define Required Constraint for PostalCode
+            // PostalCode (Required)
             Addresses
                 .Property(a => a.PostalCode)
                 .IsRequired();
 
-            // Define Foreign Key to Barangay
+            /*  Configure Relationships  
+             *  Barangays (many-to-one)
+             *  Cities (many-to-one)
+             *  Provinces (many-to-one)
+             *  Regions (many-to-one)
+             *  UsersInfo (one-to-many)
+             */
             Addresses
                 .HasOne(a => a.Barangay)
                 .WithMany(b => b.Addresses)
                 .HasForeignKey(a => a.BarangayId)
                 .OnDelete(DeleteBehavior.SetNull);
-
-            // Define Foreign Key to City
             Addresses
                 .HasOne(a => a.City)
                 .WithMany(c => c.Addresses)
                 .HasForeignKey(a => a.CityId)
                 .OnDelete(DeleteBehavior.SetNull);
-
-            // Define Foreign Key to Province
             Addresses
                 .HasOne(a => a.Province)
                 .WithMany(p => p.Addresses)
                 .HasForeignKey(a => a.ProvinceId)
                 .OnDelete(DeleteBehavior.SetNull);
-
-            // Define Foreign Key to Region
             Addresses
                 .HasOne(a => a.Region)
                 .WithMany(r => r.Addresses)
                 .HasForeignKey(a => a.RegionId)
                 .OnDelete(DeleteBehavior.SetNull);
-
-            // Define Relationship to UserInfo
             Addresses
                 .HasMany(a => a.UsersInfo)
                 .WithOne(ui => ui.Address)
