6b3f800780126a0ce055261fc398d62e633ef9a5
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-04 23:28:00 +0800

    fix the close button in sign_up pagechore(Data): refactored UserInfo model and config and modified property configs

--- Data/Models/User/UserInfo.cs
+++ Data/Models/User/UserInfo.cs
@@ -3,29 +3,24 @@
     // UsersInfo Table
     public class UserInfo
     {
-        public int UserInfoId { get; set; } // Primary Key        
-        public string FirstName { get; set; } = string.Empty;   // Required
-        public string? MiddleName { get; set; } 
-        public string LastName { get; set; } = string.Empty;    // Required
-        public string? Suffix { get; set; }
-        public int Age { get; set; }    // Required
-        public char Sex { get; set; }   // Required
+        public int UserInfoId { get; set; }                                 // Primary Key        
+        public string FirstName { get; set; } = string.Empty;               // Required; Max Length : 50
+        public string? MiddleName { get; set; }                             // Optional; Max Length : 50
+        public string LastName { get; set; } = string.Empty;                // Required; Max Length : 50
+        public string? Suffix { get; set; }                                 // Optional; Max Length : 10
+        public int Age { get; set; }                                        // Required
+        public string Sex { get; set; } = string.Empty;                     // Required; Max Length : 10
+        public int BirthInfoId { get; set; }                                // Foreign Key to BirthInfo
+        public int AddressId { get; set; }                                  // Foreign Key to Address
+        public string ContactNumber { get; set; } = string.Empty;           // Required; Field Length : 11
+        public string TaxIdentificationNumber { get; set; } = string.Empty; // Required; Max Length : 12
+        public string CivilStatus { get; set; } = string.Empty;             // Required; Max Length : 20
+        public string Religion { get; set; } = string.Empty;                // Required; Max Length : 50
 
 
-        public int BirthInfoId { get; set; } // Foreign Key to BirthInfo
-        public BirthInfo BirthInfo { get; set; } = null!;   // Navigation Property
-
-
-        public int AddressId { get; set; }  // Foreign Key to Address
-        public Address Address { get; set; } = null!;   // Navigation Property
-
-
-        public string ContactNumber { get; set; } = string.Empty;   // Required
-        public string TaxIdentificationNumber { get; set; } = string.Empty; // Required
-        public string CivilStatus { get; set; } = string.Empty; // Required
-        public string Religion { get; set; } = string.Empty;    // Required
-
-
-        public ICollection<CustomerAuth> CustomersAuth { get; set; } = null!; // Navigation Property
+        /*  Navigation Properties   */
+        public BirthInfo BirthInfo { get; set; } = null!;
+        public Address Address { get; set; } = null!;
+        public ICollection<CustomerAuth> CustomersAuth { get; set; } = null!; 
     }
 }
