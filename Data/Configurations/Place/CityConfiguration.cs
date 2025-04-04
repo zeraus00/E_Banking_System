0d1bf638144b3459780e32cf9862fc4ef77033ec
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-04 22:19:11 +0800

    chore(Data): added one-to-many relationship with Addresses and one-to-many relationship with BirthsInfo to Cities model config

--- Data/Configurations/Place/CityConfiguration.cs
+++ Data/Configurations/Place/CityConfiguration.cs
@@ -1,4 +1,4 @@
-﻿namespace E_BankingSystem.Data.Configurations.Place
+﻿namespace Data.Configurations.Place
 {
     public class CityConfiguration : IEntityTypeConfiguration<City>
     {
@@ -6,25 +6,39 @@
         public void Configure(EntityTypeBuilder<City> Cities)
         {
             Cities.ToTable("Cities", "Place");
-            // Define Primary Key
+            /*  Configure Table Properties  */
+
+            // CityId (Primary Key)
             Cities
                 .HasKey(c => c.CityId);
             Cities
                 .Property(c => c.CityId)
                 .ValueGeneratedOnAdd();
-            // Define Required Constraint for CityName
+            // CityName (Required; MaxLength=50)
             Cities
                 .Property(c => c.CityName)
-                .IsRequired();
-            // Define Foreign Key to Province
+                .IsRequired()
+                .HasMaxLength(50);
+            /*  Configure Relationships 
+             *  Provinces (many-to-one)
+             *  Barangays (one-to-many)
+             *  Addresses (one-to-many)
+             *  BirthsInfo (one-to-many)
+             */
             Cities
                 .HasOne(c => c.Province)
                 .WithMany(p => p.Cities)
                 .HasForeignKey(c => c.ProvinceId);
-
-            // Define Relationship to Barangays
             Cities
                 .HasMany(c => c.Barangays)
+                .WithOne(b => b.City)
+                .HasForeignKey(b => b.CityId);
+            Cities
+                .HasMany(c => c.Addresses)
+                .WithOne(a => a.City)
+                .HasForeignKey(a => a.CityId);
+            Cities
+                .HasMany(c => c.BirthInfos)
                 .WithOne(b => b.City)
                 .HasForeignKey(b => b.CityId);
         }
