55a66da2f68c48bd2b39834883c5b0f6292638b0
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-06 19:52:38 +0800

    chore(Data): updated property configs of Loan model

--- Data/Configurations/Finance/LoanConfiguration.cs
+++ Data/Configurations/Finance/LoanConfiguration.cs
@@ -2,29 +2,100 @@
 {
     public class LoanConfiguration : IEntityTypeConfiguration<Loan>
     {
+        //  Configure Loans Table
         public void Configure(EntityTypeBuilder<Loan> Loans) 
         {
             Loans.ToTable("Loans", "Finance");
 
-            Loans.HasKey(al => al.LoanId);
+            /*  Configure Table Properties  */
 
-            Loans.Property(al => al.LoanId)
+            //  LoanId (Primary Key)
+            Loans
+                .HasKey(l => l.LoanId);
+            Loans
+                .Property(l => l.LoanId)
                 .ValueGeneratedOnAdd();
 
-            Loans.Property(al => al.AccountId).IsRequired();
+            //  AccountId (Foreign Key to Accounts Table)
+            Loans
+                .Property(l => l.AccountId)
+                .IsRequired();
 
-            Loans.Property(al => al.LoanAmount).IsRequired()
+            //  LoanTypeId (Foreign Key to LoanTypes Table)
+            Loans
+                .Property(l => l.AccountId)
+                .IsRequired();
+
+            //  LoanAmount (Required; Decimal(18,2))
+            Loans
+                .Property(l => l.LoanAmount)
+                .IsRequired()
                 .HasColumnType("DECIMAL (18, 2)");
 
-            Loans.Property(al => al.LoanTermMonths).IsRequired();
+            //  InterestRate (Required; Decimal(5,2))
+            Loans
+                .Property(l => l.InterestRate)
+                .IsRequired()
+                .HasColumnType("DECIMAL (5, 2)");
 
-            Loans.Property(al => al.StartDate).IsRequired();
+            //  LoanTermMonths (Required; Integer)
+            Loans
+                .Property(l => l.LoanTermMonths)
+                .IsRequired();
 
-            Loans.Property(al => al.EndDate).IsRequired();
+            //  MonthlyPayment (Required; Decimal(18,2))
+            Loans
+                .Property(l => l.MonthlyPayment)
+                .IsRequired()
+                .HasColumnType("DECIMAL (18, 2)");
 
-            Loans.HasOne(al => al.Account)
+            //  RemainingLoanBalance (Required; Decimal(18,2))
+            Loans
+                .Property(l => l.RemainingLoanBalance)
+                .IsRequired()
+                .HasColumnType("DECIMAL (18, 2)");
+
+            //  ApplicationDate (Required; Default: CURDATE())
+            Loans
+                .Property(l => l.ApplicationDate)
+                .IsRequired()
+                .HasDefaultValueSql("CURDATE()");
+
+            //  LoanStatus (Required; Default: 'Pending')
+            Loans
+                .Property(l => l.LoanStatus)
+                .IsRequired()
+                .HasDefaultValue("Pending");
+
+            //  StartDate (Required; DateTime)
+            Loans
+                .Property(l => l.StartDate)
+                .IsRequired();
+
+            //  DueDate (Required; DateTime)
+            Loans
+                .Property(l => l.DueDate)
+                .IsRequired();
+
+            //  UpdateDate (Required; DateTime)
+            Loans
+                .Property(l => l.UpdateDate)
+                .IsRequired();
+
+            //  EndDate (Required; DateTime)
+            Loans
+                .Property(l => l.EndDate)
+                .IsRequired();
+
+            /*  Configure Relationships  
+             *  Accounts (many-to-one)
+             *  LoanTypes (many-to-one)
+             *  
+             */
+
+            Loans.HasOne(l => l.Account)
                 .WithMany(a => a.ActiveLoans)
-                .HasForeignKey(al => al.AccountId)
+                .HasForeignKey(l => l.AccountId)
                 .OnDelete(DeleteBehavior.Restrict);
 
         }
