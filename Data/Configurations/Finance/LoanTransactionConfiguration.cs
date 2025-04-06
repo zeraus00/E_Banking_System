5f184130fc865d07ec27e8a1874a47fd8acdd4cb
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-06 18:49:38 +0800

    Refactor(Data): refactored LoanTransactionConfiguration

--- Data/Configurations/Finance/LoanTransactionConfiguration.cs
+++ Data/Configurations/Finance/LoanTransactionConfiguration.cs
@@ -2,28 +2,46 @@
 {
     public class LoanTransactionConfiguration : IEntityTypeConfiguration<LoanTransaction>
     {
+        //  Configure LoanTransactionTable
         public void Configure (EntityTypeBuilder<LoanTransaction> LoanTransactions) 
         {
             LoanTransactions
                 .ToTable("LoanTransactions", "Finance");
 
+            /*  Configure Table Properties  */
+
+            //  LoanTransactionId (Primary Key)
             LoanTransactions
                 .HasKey(lt => lt.LoanTransactionId);
 
             LoanTransactions
                 .Property(lt => lt.LoanTransactionId).IsRequired();
 
+            //  AmountPaid (Required; Decimal(18,2))
+
+            //  RemainingLoanBalance (Required; Decimal(18,2))
             LoanTransactions
                 .Property(lt => lt.RemainingLoanBalance).IsRequired()
                 .HasColumnType("DECIMAL (18, 2)");
 
+            //  InterestAmount (Required; Decimal(18,2))
             LoanTransactions
                 .Property(lt => lt.InterestAmount).IsRequired()
                 .HasColumnType("DECIMAL (18, 2)");
 
+            //  PrincipalAmount (Required; Decimal(18,2))
             LoanTransactions
                 .Property(lt => lt.PrincipalAmount).IsRequired()
                 .HasColumnType("DECIMAL (18, 2)");
+
+
+
+            /*
+             * Configure Relationships
+             * Accounts (many-to-one)
+             * ActiveLoans (many-to-one)
+             * LoanTypes (many-to-one)
+             */
 
             LoanTransactions
                 .HasOne(lt => lt.ActiveLoans)
