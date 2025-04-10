c0d75b91df5308404ee22f45bff739ce5f202424
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-10 15:56:11 +0800

    chore(Data): remove private set constraint in Account model fields

--- Data/Models/Finance/Account.cs
+++ Data/Models/Finance/Account.cs
@@ -4,20 +4,20 @@
     // Accounts Table
     public class Account
     {
-        public int AccountId { get; private set; }                          // Primary Key
-        public string AccountType { get; private set; } = string.Empty;     // Required; Checking, Savings, etc.
-        public string AccountNumber { get; private set; } = string.Empty;   // Required
-        public string AccountName { get; private set; } = string.Empty;     // Required; MaxLength=30
-        public string AccountStatus { get; private set; } = string.Empty;   // Required; Open, Closed, Suspended
-        public decimal Balance { get; private set; } = 0.0m;                // Required; Default 0.0
+        public int AccountId { get; set; }                          // Primary Key
+        public string AccountType { get; set; } = string.Empty;     // Required; Checking, Savings, etc.
+        public string AccountNumber { get; set; } = string.Empty;   // Required
+        public string AccountName { get; set; } = string.Empty;     // Required; MaxLength=30
+        public string AccountStatus { get; set; } = string.Empty;   // Required; Open, Closed, Suspended
+        public decimal Balance { get; set; } = 0.0m;                // Required; Default 0.0
 
-        public DateTime DateOpened { get; private set; } = DateTime.UtcNow; // Required; Default DateTime.UtcNow
-        public DateTime? DateClosed { get; private set; }
+        public DateTime DateOpened { get; set; } = DateTime.UtcNow; // Required; Default DateTime.UtcNow
+        public DateTime? DateClosed { get; set; }
 
 
-        public ICollection<CustomerAuth> CustomersAuth { get; private set; } = null!; 
-        public ICollection<Transaction> Transactions { get; private set; } = null!;  
-        public ICollection<Loan> Loans { get; private set; } = null!; 
+        public ICollection<CustomerAuth> CustomersAuth { get; set; } = null!; 
+        public ICollection<Transaction> Transactions { get; set; } = null!;  
+        public ICollection<Loan> Loans { get; set; } = null!; 
         public ICollection<LoanTransaction> LoanTransactions { get; set; } = null!; 
     }
         
