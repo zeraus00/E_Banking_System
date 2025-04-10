d64a0b60262b82c82a76e41946084119ffdb40be
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-10 11:34:47 +0800

    chore(Data): converted BirthInfoId and AddressId in UserInfo model to nullable

--- Data/Models/User/UserInfo.cs
+++ Data/Models/User/UserInfo.cs
@@ -10,8 +10,8 @@
         public string? Suffix { get; set; }                                 // Optional; Max Length : 10
         public int Age { get; set; }                                        // Required
         public string Sex { get; set; } = string.Empty;                     // Required; Max Length : 10
-        public int BirthInfoId { get; set; }                                // Foreign Key to BirthInfo
-        public int AddressId { get; set; }                                  // Foreign Key to Address
+        public int? BirthInfoId { get; set; }                               // Foreign Key to BirthInfo
+        public int? AddressId { get; set; }                                 // Foreign Key to Address
         public string ContactNumber { get; set; } = string.Empty;           // Required; Field Length : 11
         public string TaxIdentificationNumber { get; set; } = string.Empty; // Required; Max Length : 12
         public string CivilStatus { get; set; } = string.Empty;             // Required; Max Length : 20
