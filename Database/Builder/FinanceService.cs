a99b149a0648ad28afa82bef75ba888b4894a7f8
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-10 15:57:16 +0800

    feat(Database): introduce AddAccount task in FinanceService

--- Database/Builder/FinanceService.cs
+++ Database/Builder/FinanceService.cs
@@ -3,5 +3,78 @@
     public class FinanceService(DbContext context)
     {
         DbContext _context = context;
+
+        public async Task AddAccount(Account account)
+        {
+            await _context.Set<Account>().AddAsync(account);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public class AccountBuilder
+    {
+        private string _accountType = string.Empty;
+        private string _accountNumber = string.Empty;
+        private string _accountName = string.Empty;
+        private string _accountStatus = string.Empty;
+        private decimal _balance = 0.0m;
+        private DateTime _dateOpened = DateTime.UtcNow;
+        private DateTime? _dateClosed;
+
+        public AccountBuilder WithAccountType(string accountType)
+        {
+            _accountType = accountType;
+            return this;
+        }
+
+        public AccountBuilder WithAccountNumber(string accountNumber)
+        {
+            _accountNumber = accountNumber;
+            return this;
+        }
+
+        public AccountBuilder WithAccountName(string accountName)
+        {
+            _accountName = accountName;
+            return this;
+        }
+
+        public AccountBuilder WithAccountStatus(string accountStatus)
+        {
+            _accountStatus = accountStatus;
+            return this;
+        }
+
+        public AccountBuilder WithBalance(decimal balance)
+        {
+            _balance = balance;
+            return this;
+        }
+
+        public AccountBuilder WithDateOpened(DateTime dateOpened)
+        {
+            _dateOpened = dateOpened;
+            return this;
+        }
+
+        public AccountBuilder WithDateClosed(DateTime? dateClosed)
+        {
+            _dateClosed = dateClosed;
+            return this;
+        }
+
+        public Account Builder()
+        {
+            return new Account
+            {
+                AccountType = _accountType,
+                AccountNumber = _accountNumber,
+                AccountName = _accountName,
+                AccountStatus = _accountStatus,
+                Balance = _balance,
+                DateOpened = _dateOpened,
+                DateClosed = _dateClosed
+            };
+        }
     }
 }
