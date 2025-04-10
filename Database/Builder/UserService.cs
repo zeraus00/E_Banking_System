3d17bb5cfce710c04c85428c899cb10c33e3e585
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-10 15:43:15 +0800

    feat(Data): introduce builder classes for UserSchema

--- Database/Builder/UserService.cs
+++ Database/Builder/UserService.cs
@@ -1,21 +1,29 @@
 ﻿namespace Database.Builder
 {
-    public class UserService
+    public class UserService(DbContext context)
     {
-        private readonly DbContext _context;
-        public UserService(DbContext context)
+        private readonly DbContext _context = context;
+
+        public async Task AddUserInfo(UserInfo userInfo)
         {
-            _context = context;
+            await _context.Set<UserInfo>().AddAsync(userInfo);
+            await _context.SaveChangesAsync();
         }
 
-        public void AddUserInfo(UserInfo userInfo)
+        public async Task AddBirthInfo(BirthInfo birthInfo)
         {
-            _context.Set<UserInfo>().Add(userInfo);
-            _context.SaveChanges();
+            await _context.Set<BirthInfo>().AddAsync(birthInfo);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task AddAddress(Address address)
+        {
+            await _context.Set<Address>().AddAsync(address);
+            await _context.SaveChangesAsync();
         }
     }
 
-    class UserInfoBuilder
+    public class UserInfoBuilder
     {
         private string _firstName = string.Empty;
         private string? _middleName;
@@ -118,6 +126,116 @@
                 TaxIdentificationNumber = _taxIdentificationNumber,
                 CivilStatus = _civilStatus,
                 Religion = _religion
+            };
+        }
+    }
+
+    public class BirthInfoBuilder
+    {
+        private DateTime _birthDate;
+        private int _cityId;
+        private int _provinceId;
+        private int _regionId;
+
+        public BirthInfoBuilder WithBirthDate(DateTime birthDate)
+        {
+            _birthDate = birthDate;
+            return this;
+        }
+
+        public BirthInfoBuilder WithCityId(int cityId)
+        {
+            _cityId = cityId;
+            return this;
+        }
+
+        public BirthInfoBuilder WithProvinceId(int provinceId)
+        {
+            _provinceId = provinceId;
+            return this;
+        }
+
+        public BirthInfoBuilder WithRegionId(int regionId)
+        {
+            _regionId = regionId;
+            return this;
+        }
+
+        public BirthInfo Build()
+        {
+            return new BirthInfo
+            {
+                BirthDate = _birthDate,
+                CityId = _cityId,
+                ProvinceId = _provinceId,
+                RegionId = _regionId
+            };
+        }
+    }
+
+    public class AddressBuilder
+    {
+        private string _house = string.Empty;
+        private string _street = string.Empty;
+        private int _barangayId;
+        private int _cityId;
+        private int _provinceId;
+        private int _regionId;
+        private int _postalCode;
+
+        public AddressBuilder WithHouse(string house)
+        {
+            _house = house;
+            return this;
+        }
+
+        public AddressBuilder WithStreet(string street)
+        {
+            _street = street;
+            return this;
+        }
+
+        public AddressBuilder WithBarangayId(int barangayId)
+        {
+            _barangayId = barangayId;
+            return this;
+        }
+
+        public AddressBuilder WithCityId(int cityId)
+        {
+            _cityId = cityId;
+            return this;
+        }
+
+        public AddressBuilder WithProvinceId(int provinceId)
+        {
+            _provinceId = provinceId;
+            return this;
+        }
+
+        public AddressBuilder WithRegionId(int regionId)
+        {
+            _regionId = regionId;
+            return this;
+        }
+
+        public AddressBuilder WithPostalCode(int postalCode)
+        {
+            _postalCode = postalCode;
+            return this;
+        }
+
+        public Address Build()
+        {
+            return new Address
+            {
+                House = _house,
+                Street = _street,
+                BarangayId = _barangayId,
+                CityId = _cityId,
+                ProvinceId = _provinceId,
+                RegionId = _regionId,
+                PostalCode = _postalCode
             };
         }
     }
