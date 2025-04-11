b947aa3e7fff2ebc43dc792c48b6eb0602031e18
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-11 22:16:05 +0800

    wip(Service): added Authentication logic (temporary)

--- Service/AuthenticationService.cs
+++ Service/AuthenticationService.cs
@@ -1,7 +1,59 @@
-﻿namespace Service
-{
-    public class AuthenticationService
-    {
+﻿using Exceptions;
 
+namespace Service
+{
+    public class AuthenticationService(DbContext context)
+    {
+        private readonly DbContext _context = context;
+        // create a passwordhasher object here!
+
+        public async Task<bool> IsAuthenticated(string Email, string Password)
+        {
+            try
+            {
+                // check if email exists
+                bool emailExists = await this.EmailExists(Email);
+                if (!emailExists)
+                {
+                    throw new UserNotFoundException();
+                }
+
+                bool passwordIsCorrect = await this.PasswordIsCorrect(Email, Password);
+                if (!passwordIsCorrect)
+                {
+                    throw new UserNotFoundException();
+                }
+
+                return true;
+            }
+            catch (UserNotFoundException)
+            {
+                return false;
+            }
+
+            //verify password
+        }
+
+        // verify if email exists
+        public async Task<bool> EmailExists(string email)
+        {
+            var user = await _context.Set<CustomerAuth>().FirstOrDefaultAsync(u => u.Email == email);
+            return user != null;
+        }
+
+        /* temporary method for testing authentication */
+        public async Task<bool> PasswordIsCorrect(string email, string password)
+        {
+            var user = await _context.Set<CustomerAuth>().FirstAsync(u => u.Email == email);
+            if (password == user.Password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
     }
 }
