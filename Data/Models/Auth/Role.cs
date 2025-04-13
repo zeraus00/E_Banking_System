82b32eb64929413436d2a62edc00dce40e27aaad
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-13 16:59:14 +0800

    fix(DB): fixed RoleName private set in Roles table

--- Data/Models/Auth/Role.cs
+++ Data/Models/Auth/Role.cs
@@ -5,7 +5,7 @@
     {
         /*  Properties  */
         public int RoleId { get; private set; } // Primary Key
-        public string RoleName { get; private set; } = string.Empty;   // Required; Customer, Employee, Admin
+        public string RoleName { get; set; } = string.Empty;   // Required; Customer, Employee, Admin
 
         /*  Navigation Properties  */
         public ICollection<UserAuth> UsersAuth { get; set; } = new List<UserAuth>();
