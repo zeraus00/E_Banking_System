5728c44f0e55307f004fc25f123d37c6188de751
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-16 00:22:00 +0800

    doc(enum): updated RoleType enum documentation

--- Data/Enums/RoleType.cs
+++ Data/Enums/RoleType.cs
@@ -2,6 +2,7 @@
 {
     /// <summary>
     /// An enum object containing the Roles in the Roles Table and their corresponding id.
+    /// THIS IS USED FOR VALIDATION PURPOSES IN THE AUTHENTICATION SERVICE LAYER.
     /// </summary>
     public enum RoleType
     {
