4bdcbe36bd01b1837b07bdb7b17be1263e8b8642
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-16 00:33:07 +0800

    feat(authentication): added Role-Based authentication logic.

---
+++ Exceptions/RoleNotAssignedException.cs
@@ -0,0 +1,13 @@
+﻿namespace Exceptions
+{
+    /// <summary>
+    /// Thrown when a user is not assigned any role.
+    /// </summary>
+    public class RoleNotAssignedException : Exception
+    {
+        public RoleNotAssignedException() : base("User role not found.") { }
+        public RoleNotAssignedException(string userName) : base($"User {userName} role not found.") { }
+        public RoleNotAssignedException(int userId) : base($"User with id {userId} role not found.") { }
+
+    }
+}
