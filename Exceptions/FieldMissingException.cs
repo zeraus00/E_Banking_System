14e88615453f4508d93c29b59c1c889842209e78
julius-git04 <@lius.trndd.com>
2025-04-17 00:06:05 +0800

    fix registrationservice, not done yet

--- Exceptions/FieldMissingException.cs
+++ Exceptions/FieldMissingException.cs
@@ -4,40 +4,6 @@
     {
         public FieldMissingException() : base("This field is required.") { }
 
-        public FieldMissingException(string fieldName, bool CustomFieldName) : base($"{fieldName} is required.") { }
-
-        public static FieldMissingException firstName() => new FieldMissingException("First Name", true);
-
-        public static FieldMissingException middleName() => new FieldMissingException("Middle Name", true);
-
-        public static FieldMissingException lastName() => new FieldMissingException("Last Name", true);
-
-        public static FieldMissingException Age() => new FieldMissingException("Age", true);
-
-        public static FieldMissingException birthDate() => new FieldMissingException("Birth Date", true);
-
-        public static FieldMissingException birthPlace() => new FieldMissingException("Birth Place", true);
-
-        public static FieldMissingException Sex() => new FieldMissingException("Sex", true);
-
-        public static FieldMissingException civilStatus() => new FieldMissingException("Civil Status", true);
-
-        public static FieldMissingException Religion() => new FieldMissingException("Religion", true);
-
-        public static FieldMissingException Region() => new FieldMissingException("Region", true);
-        public static FieldMissingException Province() => new FieldMissingException("Province", true);
-        public static FieldMissingException City() => new FieldMissingException("City", true);
-        public static FieldMissingException Barangay() => new FieldMissingException("Barangay", true);
-        public static FieldMissingException PostalCode() => new FieldMissingException("Postal Code", true);
-        public static FieldMissingException streetNo() => new FieldMissingException("Street No.", true);
-        public static FieldMissingException houseNo() => new FieldMissingException("House No.", true);
-        public static FieldMissingException Email() => new FieldMissingException("Email", true);
-        public static FieldMissingException contactNo() => new FieldMissingException("Contact No.", true);
-        public static FieldMissingException fatherFirstName() => new FieldMissingException("Father's First Name", true);
-        public static FieldMissingException fatherLastName() => new FieldMissingException("Father's Last Name", true);
-        public static FieldMissingException motherFirstName() => new FieldMissingException("Mother's First Name", true);
-        public static FieldMissingException motherLastName() => new FieldMissingException("Mother's Last Name", true);
-        public static FieldMissingException validId() => new FieldMissingException("Government Valid ID", true);
-        public static FieldMissingException Picture1x1() => new FieldMissingException("Picture 1x1", true);
+        public FieldMissingException(string fieldName) : base($"{fieldName} is required.") { }
     }
 }
---
+++ Services/RegistrationService.cs
@@ -0,0 +1,110 @@
+﻿using Exceptions;
+using Data;
+using Data.Repositories.User;
+
+
+namespace Services
+{
+    public class RegistrationService : Service
+    {
+        NameRepository _nameRepository;
+        UserInfoRepository _userInfoRepository;
+        UserInfoBuilder _userInfoBuilder;
+
+
+        public RegistrationService(EBankingContext context) : base(context)
+        {
+            _userInfoRepository = new UserInfoRepository(_context);
+            _nameRepository = new NameRepository(_context);
+            _userInfoBuilder = new UserInfoBuilder();
+        }
+
+
+        public async Task RegisterAsync(string userFirstName, string? userMiddleName, string userLastName)
+        {
+            Name UserName = await RegisterName("Lando", "Alon", "Bogart", null);
+            _userInfoBuilder
+                .WithUserNameId(UserName.NameId);
+
+            Name MotherName = await RegisterName("Lily", "Pronda", "Bogart", null);
+            _userInfoBuilder
+                .WithUserNameId(UserName.NameId);
+
+            Name FatherName = await RegisterName("Pando", "Pronda", "Bogart", null);
+            _userInfoBuilder
+                .WithUserNameId(UserName.NameId);
+        }
+
+
+        public async Task<Name> RegisterName(string firstName, string? middleName, string lastName, string? suffix)
+        {
+
+            if (string.IsNullOrWhiteSpace(firstName)) 
+            {
+                throw new FieldMissingException($"{firstName}");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName)) 
+            {
+                throw new FieldMissingException($"{lastName}");
+            }
+
+            var nameBuilder = new NameBuilder();
+            nameBuilder
+                .WithFirstName(firstName)
+                .WithLastName(lastName);
+
+            if (!string.IsNullOrWhiteSpace(middleName))
+            {
+                nameBuilder.WithMiddleName(middleName);
+            }
+
+            if (!string.IsNullOrWhiteSpace(suffix))
+            {
+                nameBuilder.WithSuffix(suffix);
+            }
+
+            Name UserName = nameBuilder.Build();
+            await _nameRepository.AddAsync(UserName);
+            await _nameRepository.SaveChangesAsync();
+            return UserName;
+        }
+
+        public async Task<BirthInfo> RegisterBirthInfo(DateTime birthDate, int cityId, int provinceId, int regiodId) 
+        {
+            if (birthDate == default) 
+            {
+                throw new FieldMissingException();
+            }
+
+            if (cityId <= 0) 
+            {
+                throw new FieldMissingException();
+            }
+
+            if (provinceId <= 0) 
+            {
+                throw new FieldMissingException();
+            }
+
+            if (regiodId <= 0) 
+            {
+                throw new FieldMissingException();
+            }
+
+            var birthInfoBuilder = new BirthInfoBuilder()
+            .WithBirthDate(birthDate)
+            .WithCityId(cityId)
+            .WithProvinceId(provinceId)
+            .WithRegionId(regiodId);
+
+            var birthInfo = birthInfoBuilder.Build();
+
+            var birthInfoRepository = new BirthInfoRepository(_context);
+            await birthInfoRepository.AddAsync(birthInfo);
+            await birthInfoRepository.SaveChangesAsync();
+
+            return birthInfo;
+        }
+    }
+}
