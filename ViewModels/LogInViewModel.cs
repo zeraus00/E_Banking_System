1cdeed6229b73e2adf0ce08a5c12d6f3ab2976aa
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-18 15:22:35 +0800

    feat(login): added viewmodel for login

---
+++ ViewModels/LogInViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels
+{
+    public class LogInViewModel
+    {
+        [Required(AllowEmptyStrings=false, ErrorMessage = "Please provide an Email.")]
+        public string? Email { get; set; }
+        [Required(AllowEmptyStrings=false, ErrorMessage = "Please provide a Password.")]
+        public string? Password { get; set; }
+    }
+}
