d3ba2c939d58556033b4736b268221bfe4196311
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-20 01:52:28 +0800

    feat(service): - add custom authenticationservice and authenticationstateprovider

---
+++ Services/NexusAuthenticationService.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Claims;
+
+namespace Services
+{
+    public class NexusAuthenticationService
+    {
+        public event Action<ClaimsPrincipal>? UserChanged;
+        private ClaimsPrincipal? _currentUser;
+
+        public ClaimsPrincipal currentUser
+        {
+            get { return _currentUser ?? new(); }
+            set
+            {
+                currentUser = value;
+                if (UserChanged != null)
+                {
+                    UserChanged(currentUser);
+                }
+            }
+        }
+    }
+}
