e00c2a865942ee6e40a3792e3da0bb44a6cff15a
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-19 18:17:22 +0800

    feat(data): - add constants for custom claim types - add constants for role names

---
+++ Data/Constants/RoleNames.cs
@@ -0,0 +1,9 @@
+﻿namespace Data.Constants
+{
+    public static class RoleNames
+    {
+        public const string Administrator = "Administrator";
+        public const string User = "User";
+        public const string Employee = "Employee";
+    }
+}
