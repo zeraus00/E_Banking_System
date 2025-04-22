0445abff9828bb9e13b9d66ddcea899fa3fd5162
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-22 15:54:50 +0800

    wip(contact no change): - add contact no change method wip

---
+++ ViewModels/ContactNumberChangeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels
+{
+    public class ContactNumberChangeViewModel
+    {
+        [Required(ErrorMessage = "Please provide a number.")]
+        public string? ContactNew { get; set; }
+        [Required(ErrorMessage = "Please provide a number.")]
+        public string? ContactConfirm { get; set; }
+    }
+}
