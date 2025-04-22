c47c837463cb8421e444624900d86cd9d38587b2
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-22 15:11:52 +0800

    feat(viewModel): - added EmailAddress annotation to email property.

--- ViewModels/LogInViewModel.cs
+++ ViewModels/LogInViewModel.cs
@@ -5,6 +5,7 @@ namespace ViewModels
     public class LogInViewModel
     {
         [Required(AllowEmptyStrings=false, ErrorMessage = "Please provide an Email.")]
+        [EmailAddress]
         public string? Email { get; set; }
         [Required(AllowEmptyStrings=false, ErrorMessage = "Please provide a Password.")]
         public string? Password { get; set; }
