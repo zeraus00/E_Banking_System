7b7a58279ad18bff4e7dff2608482aa70a14ba6d
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-22 15:38:10 +0800

    feat(viewmodel): - add email change view model - update all relevant  references.

---
+++ ViewModels/EmailChangeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViewModels
+{
+    public class EmailChangeViewModel
+    {
+        [Required(AllowEmptyStrings=false, ErrorMessage = "Please provide an Email.")]
+        [EmailAddress]
+        public string? EmailNew { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please provide an Email.")]
+        [EmailAddress]
+        public string? EmailConfirm { get; set; }
+
+    }
+}
