92c35d25b701a5d8789e3a4f37934c501ca84e10
Daina Parducho <dainajoyperolina@gmail.com>
2025-04-23 21:59:30 +0800

    refactor: move FormatHelper class to a separate file

---
+++ Helpers/FormatHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Helpers
+{
+    public static class FormatHelper
+    {
+        public static string PhoneNumberFormatter(string phone)
+        {
+            if (string.IsNullOrWhiteSpace(phone) || phone.Length < 10)
+            {
+                return phone;
+            }
+            return Regex.Replace(phone, @"(\d{4})(\d{3})(\d+)", "$1 $2 $3");
+        }
+
+        public static string AccountNumberFormatter(string accountNumber)
+        {
+            if (string.IsNullOrWhiteSpace(accountNumber) || accountNumber.Length < 10)
+            {
+                return accountNumber;
+            }
+            return Regex.Replace(accountNumber, @"(\d{3})(\d{3})(\d{3})(\w+)", "$1 $2 $3 $4");
+        }
+    }
+}
