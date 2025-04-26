5902fe372fd186b0901200dc8d574ad9922c106c
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-26 22:08:54 +0800

    feat(data): add list in enums for AccountProductTypeNames and AccountTypeNames

---
+++ Data/Constants/AccountProductTypeNames.cs
@@ -0,0 +1,16 @@
+﻿namespace Data.Constants
+{
+    public class AccountProductTypeNames
+    {
+        public const string Savings = "Savings";
+        public const string Checking = "Checking";
+        public const string Loan = "Loan";
+
+        public readonly List<string> AccountProductTypeNameList = new()
+        {
+            Savings,
+            Checking,
+            Loan
+        };
+    }
+}
