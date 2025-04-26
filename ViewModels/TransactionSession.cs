0cec5421fa36bd1276bb84baaf7a0104b9933777
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-26 11:11:41 +0800

    feat(session): - add session viewmodel for transaction session

---
+++ ViewModels/TransactionSession.cs
@@ -0,0 +1,14 @@
+﻿namespace ViewModels
+{
+    public class TransactionSession
+    {
+        public int TransactionTypeId { get; set; }
+        public string TransactionNumber { get; set; } = string.Empty;
+        public int MainAccountId { get; set; }
+        public decimal Amount { get; set; }
+        public decimal CurrentBalance { get; set; }
+        public string? ConfirmationNumber { get; set; } = null;
+        public int? CounterAccountId { get; set; } = null;
+
+    }
+}
