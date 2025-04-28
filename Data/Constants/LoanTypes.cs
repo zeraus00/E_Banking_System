af89189ea954c2df7e6b8c9a79b43cbca39712fe
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-28 21:42:23 +0800

    feat(data): add loan type templates in constants

---
+++ Data/Constants/LoanTypes.cs
@@ -0,0 +1,72 @@
+﻿namespace Data.Constants
+{
+    public class LoanTypes
+    {
+        public const string PERSONAL_LOAN = "Personal Loan";
+        public const string HOME_LOAN = "Home loan";
+        public const string CAR_LOAN = "Car loan";
+        public const string SECURED_LOAN = "Secured loan";
+        public const string UNSECURED_LOAN = "Unsecured loan";
+        public const string MICROLOAN = "Microloan";
+
+        public static List<string> LOAN_TYPE_NAMES { get; } = new()
+        {
+            PERSONAL_LOAN,
+            HOME_LOAN,
+            CAR_LOAN,
+            SECURED_LOAN,
+            UNSECURED_LOAN,
+            MICROLOAN
+        };
+
+        public static LoanType PERSONAL_LOAN_TYPE { get; } = new()
+        {
+            LoanTypeName = PERSONAL_LOAN,
+            InterestRatePerAnnum = 0.10m,
+            LoanTermInMonths = 12
+        };
+
+        public static LoanType HOME_LOAN_TYPE { get; } = new()
+        {
+            LoanTypeName = HOME_LOAN,
+            InterestRatePerAnnum = 0.04m,
+            LoanTermInMonths = 120
+        };
+
+        public static LoanType CAR_LOAN_TYPE { get; } = new()
+        {
+            LoanTypeName = CAR_LOAN,
+            InterestRatePerAnnum = 0.06m,
+            LoanTermInMonths = 36
+        };
+
+        public static LoanType SECURED_LOAN_TYPE { get; } = new()
+        {
+            LoanTypeName = SECURED_LOAN,
+            InterestRatePerAnnum = 0.05m,
+            LoanTermInMonths = 12
+        };
+        public static LoanType UNSECURED_LOAN_TYPE { get; } = new()
+        {
+            LoanTypeName = UNSECURED_LOAN,
+            InterestRatePerAnnum = 0.12m,
+            LoanTermInMonths = 12
+        };
+        public static LoanType MICROLOAN_TYPE { get; } = new()
+        {
+            LoanTypeName = MICROLOAN,
+            InterestRatePerAnnum = 0.15m,
+            LoanTermInMonths = 6
+        };
+
+        public static List<LoanType> LOAN_TYPE_LIST { get; } = new()
+        {
+            PERSONAL_LOAN_TYPE,
+            HOME_LOAN_TYPE,
+            CAR_LOAN_TYPE,
+            SECURED_LOAN_TYPE,
+            UNSECURED_LOAN_TYPE,
+            MICROLOAN_TYPE
+        };
+    }
+}
