aaecd48fd370b89c9c7142de8d5aa39b38bbbfe4
Archelaus Agaton <archelausagaton@gmail.com>
2025-04-28 11:15:46 +0800

    chore(constants): add routes to PageRoutes

--- Data/Constants/PageRoutes.cs
+++ Data/Constants/PageRoutes.cs
@@ -2,22 +2,27 @@
 {
     public class PageRoutes
     {
-        public const string LANDING_PAGE = "/";
-        public const string LOG_IN_PAGE = "/Login_page";
-        public const string LOG_OUT_PAGE = "/Logout_page";
         public const string ACCOUNT_OPENING = "/Account_opening";
         public const string CLIENT_HOME = "/Client_home";
-        public const string MY_ACCOUNTS = "/My_accounts";
-        public const string MANAGE_ACCOUNTS = "/Manage_accounts";
-        public const string WITHDRAW_AMOUNT = "/Withdraw_amount";
-        public const string WITHDRAW_CONFIRMATION = "/Withdraw_confirmation";
-        public const string WITHDRAW_RECEIPT = "/Withdraw_receipt";
         public const string DEPOSIT_AMOUNT = "/Deposit_amount";
         public const string DEPOSIT_CONFIRMATION = "/Deposit_confirmation";
         public const string DEPOSIT_RECEIPT = "/Deposit_receipt";
+        public const string LANDING_PAGE = "/";
+        public const string LOG_IN_PAGE = "/Login_page";
+        public const string LOGGING_IN_PAGE = "/Logging_in";
+        public const string LOG_OUT_PAGE = "/Logout_page";
+        public const string MY_ACCOUNTS = "/My_accounts";
+        public const string MANAGE_ACCOUNTS = "/Manage_accounts";
+        public const string SETTINGS_EMAIL_PASSOWRD = "/Settings_email_password";
+        public const string SETTINGS_CHANGE_CONTACT = "/Settings_change_contact";
+        public const string SETTINGS_CHANGE_EMAIL = "/Settings_change_email";
+        public const string SETTINGS_PAGE = "/Settings_page";
         public const string TRANSACTIONS = "/Transaction";
         public const string TRANSFER_AMOUNT = "/Transfer_amount";
         public const string TRANSFER_CONFIRMATION = "/Transfer_confirmation";
         public const string TRANSFER_RECEIPT = "/Transfer_receipt";
+        public const string WITHDRAW_AMOUNT = "/Withdraw_amount";
+        public const string WITHDRAW_CONFIRMATION = "/Withdraw_confirmation";
+        public const string WITHDRAW_RECEIPT = "/Withdraw_receipt";
     }
 }
