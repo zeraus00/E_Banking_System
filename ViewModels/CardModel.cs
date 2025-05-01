6632c2114b0ce96a40e5557b660011177270ff55
Daina Parducho <dainajoyperolina@gmail.com>
2025-05-01 16:52:39 +0800

    feat: CardModel in ViewModels

---
+++ ViewModels/CardModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ViewModels
+{ 
+    public class CardModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string LogoUrl { get; set; } = string.Empty;
+    }
+}
