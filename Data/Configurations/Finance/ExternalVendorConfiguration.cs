b6d82538ef1aebd0e39eac50e670cc3c5cf59188
Archelaus Agaton <archelausagaton@gmail.com>
2025-05-01 17:22:52 +0800

    feat(data): add external vendors to database. - add external vendor field to transactions table

---
+++ Data/Configurations/Finance/ExternalVendorConfiguration.cs
@@ -0,0 +1,36 @@
+﻿namespace Data.Configurations.Finance
+{
+    public class ExternalVendorConfiguration : IEntityTypeConfiguration<ExternalVendor>
+    {
+        public void Configure(EntityTypeBuilder<ExternalVendor> ExternalVendors)
+        {
+            ExternalVendors.ToTable("ExternalVendors", "FinanceSchema");
+
+            /*  Configure Table Properties  */
+            ExternalVendors
+                .HasKey(ev => ev.VendorId);
+
+            ExternalVendors
+                .Property(ev => ev.VendorId)
+                .ValueGeneratedOnAdd();
+
+            ExternalVendors
+                .Property(ev => ev.VendorName)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            /*
+             *  Relationships
+             *  Transactions (one to many)
+             */
+            ExternalVendors
+                .HasData(
+                    new ExternalVendor { VendorName = "GCash" },
+                    new ExternalVendor { VendorName = "Paymaya" },
+                    new ExternalVendor { VendorName = "GoTyme" }
+                );
+
+
+        }
+    }
+}
---
+++ Data/Models/Finance/ExternalVendor.cs
@@ -0,0 +1,12 @@
+﻿namespace Data.Models.Finance
+{
+    public class ExternalVendor
+    {
+        /*  Table Properties    */
+        public int VendorId { get; set; }                           //  Primary Key
+        public string VendorName { get; set; } = string.Empty;      //  Required
+
+        /*  Navigation Properties   */
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
