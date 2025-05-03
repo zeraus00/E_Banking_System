882471509dd16c8299ff5019b0afa830eecfcd7f
Archelaus Agaton <archelausagaton@gmail.com>
2025-05-03 16:47:10 +0800

    fix(services): fix GenerateAccountName method in CredentialFactory class. - fix AccountTypeNameList and AccountProductTypeNameList indexing

--- Services/DataManagement/CredentialFactory.cs
+++ Services/DataManagement/CredentialFactory.cs
@@ -7,8 +7,8 @@ namespace Services.DataManagement
         public const string bankNumber = "103";
         public string GenerateAccountName(int accountTypeId, int accountProductTypeId)
         {
-            var firstPart = new AccountTypeNames().AccountTypeNameList[accountTypeId][..3];
-            var secondPart = new AccountProductTypeNames().AccountProductTypeNameList[accountProductTypeId][..3];
+            var firstPart = new AccountTypeNames().AccountTypeNameList[accountTypeId-1][..3];
+            var secondPart = new AccountProductTypeNames().AccountProductTypeNameList[accountProductTypeId-1][..3];
             return $"{firstPart}-{secondPart}-{Guid.NewGuid().ToString().Substring(0, 6).ToUpper()}";
         }
         public string GenerateAccountNumber(DateTime creationDate, int accountTypeId, int accountProductTypeId)
