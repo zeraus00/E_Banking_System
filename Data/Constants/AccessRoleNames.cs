e506f592339915c58f9d0bae58cd6e6bdadc443a
Archelaus Agaton <archelausagaton@gmail.com>
2025-05-03 20:28:50 +0800

    feat(data): add constants and enums for AccessRoles

---
+++ Data/Constants/AccessRoleNames.cs
@@ -0,0 +1,16 @@
+﻿namespace Data.Constants
+{
+    public class AccessRoleNames
+    {
+        public const string PRIMARY_OWNER = "PRIMARY OWNER";
+        public const string SECONDARY_OWNER = "SECONDARY OWNER";
+        public const string BENEFICIARY = "BENEFICIARY";
+
+        public static List<string> AsList = new List<string>
+        {
+            PRIMARY_OWNER,
+            SECONDARY_OWNER,
+            BENEFICIARY
+        };
+    }
+}
