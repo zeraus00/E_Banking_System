0c0d5360d33e04afb3c1750830112472e1dd358a
julius-git04 <@lius.trndd.com>
2025-05-03 17:55:57 +0800

    wip(add password hashing helper)

---
+++ Helpers/BcryptHelper.cs
@@ -0,0 +1,15 @@
+﻿namespace Helpers
+{
+    public class BcryptHelper
+    {
+        public static string HashPassword(string password) 
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password); 
+        }
+
+        public static bool VerifyPassword(string password, string hashedPassword) 
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
