11f58de6eee42957c8f6448e6fc0e860613f43ac
Archelaus Agaton <archelausagaton@gmail.com>
2025-05-04 17:12:09 +0800

    fix(account opening): changed api from municipality to city municipality

--- ViewModels/Places/CityViewModel.cs
+++ ViewModels/Places/CityViewModel.cs
@@ -6,6 +6,8 @@ public class CityViewModel
     public JsonElement name { get; set; }
     public JsonElement oldName { get; set; }
     public JsonElement isCapital { get; set; }
+    public JsonElement isCity { get; set; }
+    public JsonElement isMunicipality { get; set; }
     public JsonElement provinceCode { get; set; }
     public JsonElement districtCode { get; set; }
     public JsonElement regionCode { get; set; }
