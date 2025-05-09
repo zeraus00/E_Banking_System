07fe4c030d2dcc4c63fc1014aa4054c4cc5cc8e9
Archelaus Agaton <archelausagaton@gmail.com>
2025-05-09 22:56:03 +0800

    feat(data): add constants for Admin Dashboard Time Filters

---
+++ Data/Constants/AdminDashboardTimeFilters.cs
@@ -0,0 +1,11 @@
+﻿namespace Data.Constants
+{
+    public class AdminDashboardTimeFilters
+    {
+        public const string HOURLY = "hourly";
+        public const string DAILY = "daily";
+        public const string WEEKLY = "weekly";
+        public const string MONTHLY = "monthly";
+        public const string YEARLY = "yearly";
+    }
+}
