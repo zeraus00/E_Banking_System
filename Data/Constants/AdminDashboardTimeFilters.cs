1f98cf4ef35ae5c0fdcdeaed86805023e11b6b2d
Archelaus Agaton <archelausagaton@gmail.com>
2025-05-10 00:01:53 +0800

    feat(Data): add string list for dashboard time filters

--- Data/Constants/AdminDashboardTimeFilters.cs
+++ Data/Constants/AdminDashboardTimeFilters.cs
@@ -7,5 +7,14 @@
         public const string WEEKLY = "weekly";
         public const string MONTHLY = "monthly";
         public const string YEARLY = "yearly";
+
+        public static readonly List<string> AS_STRING_LIST = new()
+        {
+            HOURLY,
+            DAILY,
+            WEEKLY,
+            MONTHLY,
+            YEARLY
+        };
     }
 }
